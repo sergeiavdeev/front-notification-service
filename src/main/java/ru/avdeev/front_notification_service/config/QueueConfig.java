62679f82cba3deb819db8d2c2f9ee8eea9deaa53
sergeiavdeev <sergei_avdeev@mail.ru>
2025-02-01 16:19:26 +0300

    Queue name props

---
+++ src/main/java/ru/avdeev/front_notification_service/config/QueueConfig.java
@@ -0,0 +1,11 @@
+package ru.avdeev.front_notification_service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueueConfig {
+
+    @Value("${rabbit.queueFrontIn}")
+    public String queueFrontIn;
+}
--- src/main/java/ru/avdeev/front_notification_service/rabbit/Listener.java
+++ src/main/java/ru/avdeev/front_notification_service/rabbit/Listener.java
@@ -8,6 +8,7 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+import ru.avdeev.front_notification_service.config.QueueConfig;
 import ru.avdeev.front_notification_service.dto.Booking;
 import ru.avdeev.front_notification_service.repository.EventRepository;
 
@@ -20,8 +21,10 @@ public class Listener {
 
     private final Logger log = LoggerFactory.getLogger(Listener.class);
     private final EventRepository eventRepository;
+    private final QueueConfig queueConfig;
 
-    @RabbitListener(queues = "front.in")
+    //@RabbitListener(queues = "front.in")
+    @RabbitListener(queues = "#{queueConfig.queueFrontIn}")
     public void onMessage(Message<Booking> message) {
 
         String type = Objects.requireNonNull(message.getHeaders().get("type")).toString();

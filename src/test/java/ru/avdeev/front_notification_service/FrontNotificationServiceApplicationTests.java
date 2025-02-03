60df408f05c6c35152956811130d6f3893bc16af
sergeiavdeev <sergei_avdeev@mail.ru>
2025-02-03 22:43:41 +0300

    Clean code

--- src/test/java/ru/avdeev/front_notification_service/FrontNotificationServiceApplicationTests.java
+++ src/test/java/ru/avdeev/front_notification_service/FrontNotificationServiceApplicationTests.java
@@ -2,12 +2,14 @@ package ru.avdeev.front_notification_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class FrontNotificationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
